37c3110d6f39684f3517ad49cfc2aae401533be3
Jad Haddad <45017840+jadhaddad01@users.noreply.github.com>
2021-09-15 02:24:42 +0000

    Update top description

--- snake.py
+++ snake.py
@@ -1,8 +1,8 @@
 """
 Author: Jad Haddad
 
-flappy_bird
-https://github.com/jadhaddad01/FlappyBirdAI
+Snake
+https://github.com/jadhaddad01/SnakeAI
 Snake Artificial Intelligence
 Using the NEAT Genetic Neural Network Architecture to train a set of snakes to play the popular game Snake. Also playable by user.
 
